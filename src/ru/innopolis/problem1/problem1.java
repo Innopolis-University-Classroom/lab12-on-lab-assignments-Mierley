c814760b242b2ea2efb41a4011200c81b5a18976
Mierley <mierer.lei@gmail.com>
2022-04-13 17:28:24 +0300

    Completed

--- src/ru/innopolis/problem1/problem1.java
+++ src/ru/innopolis/problem1/problem1.java
@@ -1,2 +1,39 @@
-// your solution here
-//fix all the errors including package errors
+package ru.innopolis.problem1;
+
+abstract class Handler {
+    public void handle(){}
+}
+
+class Logger extends Handler
+{
+    protected Handler nextHandler;
+    public Logger(Logger startLogger)
+    {
+        this.nextHandler = startLogger;
+    }
+    public Logger() {}
+}
+
+class LoggerConsole extends Logger
+{
+    public LoggerConsole()
+    {
+        this.nextHandler = new LoggerFile();
+    }
+}
+
+class LoggerFile extends Logger
+{
+    public LoggerFile ()
+    {
+        this.nextHandler = new LoggerServer();
+    }
+}
+
+class LoggerServer extends Logger
+{
+    public LoggerServer ()
+    {
+        this.nextHandler = null;
+    }
+}
